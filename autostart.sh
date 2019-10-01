7fd9465113b97e303dc641622f9fdc8d3040adcd
Perrine <arwandar@gmail.com>
2019-10-01 21:40:22 +0200

    modification de l'autoStart

--- autostart.sh
+++ autostart.sh
@@ -2,4 +2,4 @@
 cd /home/pi/dev/SpotUtils/
 sudo forever stop SpotUtilsServer
 sudo npm run build
-sudo forever start -a --uid "SpotUtilsServer" ./dist/server/index.js
+sudo forever start -a --uid "SpotUtilsServer" ./dist/index.js
