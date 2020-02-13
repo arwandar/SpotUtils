0cd9ea1545f08439b94e2f65bf553d2805d36c27
Perrine SIMON <arwandar@gmail.com>
2020-02-13 11:10:03 +0100

    fix concurrence qui part en 429

--- src/getTop/index.js
+++ src/getTop/index.js
@@ -1,12 +1,17 @@
 import { getUsernames } from '../commonBDD'
 import generateTop from './generateTop'
 
+const topFor = (usernames) =>
+  generateTop(usernames.pop()).then(() =>
+    usernames.length === 0 ? Promise.resolve() : generateTop(usernames)
+  )
+
 export default (app) => {
   app.get('/api/top', (req, res) => {
     console.log('top')
 
     getUsernames()
-      .then((usernames) => Promise.all(usernames.map((username) => generateTop(username))))
+      .then((usernames) => topFor(usernames))
       .then(() => res.status(200).send('ok'))
       .catch((err) => console.error('/top', err))
   })
