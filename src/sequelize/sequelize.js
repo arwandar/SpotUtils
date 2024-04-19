560d1022ceb319a9c2d3970d54ccec94298b1978
Perrine SIMON <arwandar@gmail.com>
2024-04-19 15:50:26 +0200

    fix

--- src/sequelize/sequelize.js
+++ src/sequelize/sequelize.js
@@ -1,10 +1,13 @@
 import { Sequelize } from 'sequelize'
 import cls from 'cls-hooked'
+import fs from 'fs'
 
 const namespace = cls.createNamespace('namespace')
 Sequelize.useCLS(namespace)
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const password = fs.readFileSync(process.env.DB_PASSWORD_SECRET, 'utf8').trim()
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, password, {
   host: process.env.DB_HOST,
   dialect: 'mariadb',
   logging: false,
