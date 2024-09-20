f95bf4dbfd40ed330ef4430bcc4afbd5b29dfcc5
Perrine SIMON <perrine.simon@talosi.com>
2024-09-20 17:19:55 +0200

    try to use axios-retry

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,10 +1,19 @@
 import axios from 'axios'
+import rax from 'retry-axios'
+
+rax.attach(axios)
 
 export const customAxios = axios.create({
   baseURL: 'https://api.spotify.com/v1/',
   headers: { post: { 'Content-Type': 'application/json' } },
   proxy: false,
 })
+
+customAxios.defaults.raxConfig = {
+  instance: customAxios,
+  statusCodesToRetry: [[429, 429]],
+  retry: 5,
+}
 
 customAxios.interceptors.response.use(
   (response) => response,
